9041dcfcb565e84e372d3ae2d7367da8899afd7d
BH3GEI <ufp_enterprise@yahoo.com>
2023-11-29 08:26:59 +0000

    Some Optimizations

--- Assignment3/302/FileCopier.c
+++ Assignment3/302/FileCopier.c
@@ -9,10 +9,24 @@ void file_copy_bufsiz(const char *source, const char *destination){
     ssize_t bytes;
 
     int sourceFile = open(source, O_RDONLY);
+    if (sourceFile == -1) {
+        perror("Error opening source file");
+
+        exit(EXIT_FAILURE);
+    }
+
     int destFile = open(destination, O_WRONLY | O_CREAT, 0666);
+    if (destFile == -1) {
+        perror("Error opening destination file");
+
+        close(sourceFile);
+        exit(EXIT_FAILURE);
+    }
 
     while((bytes = read(sourceFile, buffer, BUFSIZ)) > 0){
-        write(destFile, buffer, bytes);
+        if (write(destFile, buffer, bytes) != bytes) {
+            perror("Error writing to destination file");
+        }
     }
 
     close(sourceFile);
@@ -24,10 +38,22 @@ void file_copy_char(const char *source, const char *destination){
     ssize_t bytes;
 
     int sourceFile = open(source, O_RDONLY);
+    if (sourceFile == -1) {
+        perror("Error opening source file");
+        exit(EXIT_FAILURE);
+    }
+
     int destFile = open(destination, O_WRONLY | O_CREAT, 0666);
+    if (destFile == -1) {
+        perror("Error opening destination file");
+        close(sourceFile);
+        exit(EXIT_FAILURE);
+    }
 
     while((bytes = read(sourceFile, &buffer, 1)) > 0){
-        write(destFile, &buffer, bytes);
+        if (write(destFile, &buffer, bytes) != bytes) {
+            perror("Error writing to destination file");
+        }
     }
 
     close(sourceFile);
@@ -44,12 +70,25 @@ int main(int argc, char *argv[]){
     file_copy_bufsiz(argv[1], argv[2]);
     clock_t end = clock();
 
+    char cmd[1024];
+    sprintf(cmd, "diff %s %s > /dev/null", argv[1], argv[2]);
+    if (system(cmd) != 0) {
+        fprintf(stderr, "Files %s and %s are not identical\n", argv[1], argv[2]);
+        return 1;
+    }
+
     double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
     printf("Time taken for file copy (BUFSIZ): %f seconds\n", time_spent);
 
     start = clock();
     file_copy_char(argv[1], argv[2]);
     end = clock();
+
+    sprintf(cmd, "diff %s %s > /dev/null", argv[1], argv[2]);
+    if (system(cmd) != 0) {
+        fprintf(stderr, "Files %s and %s are not identical\n", argv[1], argv[2]);
+        return 1;
+    }
 
     time_spent = (double)(end - start) / CLOCKS_PER_SEC;
     printf("Time taken for file copy (char): %f seconds\n", time_spent);
